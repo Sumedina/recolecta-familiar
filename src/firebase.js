112e71f3aaf9bcd4f477f0049578769719143bc7
Salvador <sjum1997@gmail.com>
2026-03-15 15:19:37 +0200

    Firebase keys movidas a .env y diseño responsive móvil

--- src/firebase.js
+++ src/firebase.js
@@ -12,12 +12,12 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDb7td3kBds1a2-peSCXdhZk2IGfcYkQBw",
-  authDomain: "recolecta-familiar.firebaseapp.com",
-  projectId: "recolecta-familiar",
-  storageBucket: "recolecta-familiar.firebasestorage.app",
-  messagingSenderId: "391157865533",
-  appId: "1:391157865533:web:7e101fc63a438a1085de54"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
